e435f442a0ccde2019c7c476ac811e7e892bd530
jldolader <juanlumsevilla@gmail.com>
2023-12-05 02:02:19 +0100

    ✅ COMPLETED: Day 1 - Part 2. 📝 Renamed .txt file

--- Y2023/src/Day1Part2.java
+++ Y2023/src/Day1Part2.java
@@ -1,8 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 
-public class Day1Part1 {
+public class Day1Part2 {
+
+    // Replaces number with digits but maintain 1-letter overlap
+    private static final Map<String, String> digitMap = Map.of(
+            "one", "o1e",
+            "two", "t2o",
+            "three", "t3e",
+            "four", "f4r",
+            "five", "f5e",
+            "six", "s6x",
+            "seven", "s7n",
+            "eight", "e8t",
+            "nine", "n9e"
+    );
+
     public static void main(String[] args) {
         String filePath = "Y2023/resources/Day1.txt";
 
@@ -21,6 +36,11 @@ public class Day1Part1 {
     }
 
     private static Integer treatLine(String line) {
+        for (Map.Entry<String, String> entry : digitMap.entrySet()) {
+            line = line.replaceAll(entry.getKey(), entry.getValue());
+        }
+
+
         line = line.replaceAll("[a-z]", "");
         return Integer.parseInt(line.charAt(0) + "" + line.charAt(line.length() - 1));
     }
