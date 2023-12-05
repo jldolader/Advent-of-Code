b9ccb872428b1b69868cb28e4ed51eeeeb9af254
jldolader <juanlumsevilla@gmail.com>
2023-12-05 01:41:02 +0100

    ✅ COMPLETED: Day 1 - Part 1. 📁 'resources' created.
    ▪️ Additional IntelliJ files added.

--- Y2023/src/Day1.java
+++
@@ -1,6 +0,0 @@
-public class Day1 {
-    public static void main(String[] args) {
-        // Your code goes here
-        System.out.println("Hello, World!");
-    }
-}
---
+++ Y2023/src/Day1Part1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day1Part1 {
+    public static void main(String[] args) {
+        String filePath = "Y2023/resources/Day1Part1.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int sum = 0;
+
+            while ((line = br.readLine()) != null) {
+                sum += treatLine(line);
+            }
+
+            System.out.println("The sum of all calibration values is: " + sum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Integer treatLine(String line) {
+        line = line.replaceAll("[a-z]", "");
+        return Integer.parseInt(line.charAt(0) + "" + line.charAt(line.length() - 1));
+    }
+}
